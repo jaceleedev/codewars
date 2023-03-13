81ae5164fa7d187a1c346efb3e05ea1854460c72
JaceLee <jaceleedev@gmail.com>
2023-03-13 21:57:07 +0900

    8kyu Counting sheep 풀이 완료

---
+++ 8kyu/counting-sheep.js
@@ -0,0 +1,7 @@
+// 8kyu Counting sheep... 문제
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  return arrayOfSheep.filter((sheep) => sheep === true).length
+}
