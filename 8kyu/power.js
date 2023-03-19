182fe7bc10c5a612ff21bce47ecb7308772510ca
JaceLee <jaceleedev@gmail.com>
2023-03-19 14:17:29 +0900

    8kyu Power 풀이 완료

---
+++ 8kyu/power.js
@@ -0,0 +1,12 @@
+// 8kyu Power 문제
+// https://www.codewars.com/kata/562926c855ca9fdc4800005b/train/javascript
+
+function numberToPower(number, power) {
+  let result = 1
+
+  for (let i = 0; i < power; ++i) {
+    result *= number
+  }
+
+  return result
+}
