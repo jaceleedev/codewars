bacfa8051c85aa8a7dd8ff0fc9d60fcc46238673
JaceLee <jaceleedev@gmail.com>
2023-03-19 14:14:29 +0900

    8kyu Simple multiplication 풀이 완료

---
+++ 8kyu/simple-multiplication.js
@@ -0,0 +1,7 @@
+// 8kyu Simple multiplication 문제
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+function simpleMultiplication(number) {
+  // your code........
+  return number % 2 === 0 ? number * 8 : number * 9
+}
