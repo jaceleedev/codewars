6ad2aa1b17c56e156938a59aa374cc752b1f8c52
JaceLee <jaceleedev@gmail.com>
2023-03-20 10:47:07 +0900

    8kyu Find Nearest square number 풀이 완료

---
+++ 8kyu/find-nearest-square-number.js
@@ -0,0 +1,7 @@
+// 8kyu Find Nearest square number 문제
+// https://www.codewars.com/kata/5a805d8cafa10f8b930005ba/train/javascript
+
+function nearestSq(n) {
+  // your code
+  return Math.pow(Math.round(Math.sqrt(n)), 2)
+}
