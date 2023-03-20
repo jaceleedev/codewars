72a7ba93322655895ff8e29086467c5152b00d2a
JaceLee <jaceleedev@gmail.com>
2023-03-20 10:54:42 +0900

    8kyu Is n divisible by x and y? 풀이 완료

---
+++ 8kyu/is-n-divisible-by-x-and-y.js
@@ -0,0 +1,6 @@
+// 8kyu Is n divisible by x and y? 문제
+// https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
