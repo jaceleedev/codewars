1339e8ac9e298d198374e1fdbcb41e1fe25cb63c
JaceLee <jaceleedev@gmail.com>
2023-03-21 10:28:36 +0900

    8kyu Training JS #5 풀이 완료

---
+++ 8kyu/training-js-#5.js
@@ -0,0 +1,6 @@
+// 8kyu Training JS #5: Basic data types--Object 문제
+// https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
