bd89a98a76d7f8c16af4641895c5c799005f7030
JaceLee <jaceleedev@gmail.com>
2023-03-21 10:07:01 +0900

    8kyu Training JS #1 풀이 완료

---
+++ 8kyu/training-js-#1.js
@@ -0,0 +1,9 @@
+// 8kyu Training JS #1: create your first JS function and print "Hello World!" 문제
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+
+//refer to the example and write your first JS function
+function helloWorld() {
+  const str = 'Hello World!'
+
+  console.log(str)
+}
