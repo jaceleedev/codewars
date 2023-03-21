1f4954d99829fe5a81bf70ece6b9f1086a5bcb62
JaceLee <jaceleedev@gmail.com>
2023-03-21 10:31:45 +0900

    8kyu Training JS #6 풀이 완료

---
+++ 8kyu/training-js-#6.js
@@ -0,0 +1,7 @@
+// 8kyu Training JS #6: Basic data types--Boolean and conditional statements if..else 문제
+// https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+
+function trueOrFalse(val) {
+  if (val) return 'true'
+  else return 'false'
+}
