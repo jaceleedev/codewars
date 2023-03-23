fb12c71f52b3964e354f6eb7e4f97ea179c6f7fa
JaceLee <jaceleedev@gmail.com>
2023-03-23 11:02:05 +0900

    8kyu Training JS #15 풀이 완료

---
+++ 8kyu/training-js-#15.js
@@ -0,0 +1,7 @@
+// 8 kyu Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()
+// https://www.codewars.com/kata/57256064856584bc47000611/train/javascript
+
+function howManySmaller(arr, n) {
+  //coding here..
+  return arr.filter((number) => Number(number.toFixed(2)) < n).length;
+}
