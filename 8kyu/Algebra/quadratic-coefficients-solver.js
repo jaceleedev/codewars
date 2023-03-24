924849b559c810703341b1366db4d0257a303999
JaceLee <jaceleedev@gmail.com>
2023-03-24 10:36:30 +0900

    8kyu Quadratic Coefficients Solver 풀이 완료

---
+++ 8kyu/Algebra/quadratic-coefficients-solver.js
@@ -0,0 +1,6 @@
+// 8 kyu Quadratic Coefficients Solver 문제
+// https://www.codewars.com/kata/5d59576768ba810001f1f8d6/train/javascript
+
+function quadratic(x1, x2) {
+  return [1, -(x1 + x2), x1 * x2];
+}
