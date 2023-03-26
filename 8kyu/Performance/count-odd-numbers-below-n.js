d4c26bf5da81de4d09dbee2415c8363ec3c8ec46
JaceLee <jaceleedev@gmail.com>
2023-03-26 16:03:56 +0900

    8kyu Count Odd Numbers below n 풀이 완료

---
+++ 8kyu/Performance/count-odd-numbers-below-n.js
@@ -0,0 +1,7 @@
+// 8 kyu Count Odd Numbers below n 문제
+// https://www.codewars.com/kata/59342039eb450e39970000a6/train/javascript
+
+function oddCount(n) {
+  // your code here
+  return Math.floor(n / 2)
+}
