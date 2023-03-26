834cd4799986533615e6bb20222eba2f555a0aab
JaceLee <jaceleedev@gmail.com>
2023-03-26 15:43:03 +0900

    8kyu Remove the time 풀이 완료

---
+++ 8kyu/Parsing/remove-the-time.js
@@ -0,0 +1,7 @@
+// 8 kyu Remove the time 문제
+// https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e/train/javascript
+
+function shortenToDate(longDate) {
+  // your code here
+  return longDate.split(',')[0]
+}
