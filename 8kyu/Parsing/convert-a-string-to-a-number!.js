d7be160f0baa82e70c74a4f61b34c8a6c7db4e32
JaceLee <jaceleedev@gmail.com>
2023-03-26 15:57:17 +0900

    8kyu Conver a String to a Number! 풀이 완료

---
+++ 8kyu/Parsing/convert-a-string-to-a-number!.js
@@ -0,0 +1,7 @@
+// 8 kyu Convert a String to a Number! 문제
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+const stringToNumber = function (str) {
+  // put your code here
+  return Number(str)
+}
