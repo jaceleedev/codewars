be08c7be4abc0f8c23ebd1b4288b64d6eb8d7edf
JaceLee <jaceleedev@gmail.com>
2023-03-30 13:17:20 +0900

    8kyu The Wide-Mouthed frog! 풀이 완료

---
+++ 8kyu/Logic/the-wide-mouthed-frog!.js
@@ -0,0 +1,7 @@
+// 8 kyu The Wide-Mouthed frog! 문제
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+function mouthSize(animal) {
+  // code here
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
+}
