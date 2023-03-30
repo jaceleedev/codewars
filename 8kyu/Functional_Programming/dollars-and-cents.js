d3a355d7815be8b7d7b2786538ee7498540b17fb
JaceLee <jaceleedev@gmail.com>
2023-03-30 13:06:22 +0900

    8kyu Dollars and Cents 풀이 완료

---
+++ 8kyu/Functional_Programming/dollars-and-cents.js
@@ -0,0 +1,7 @@
+// 8 kyu Dollars and Cents 문제
+// https://www.codewars.com/kata/55902c5eaa8069a5b4000083/train/javascript
+
+function formatMoney(amount) {
+  // your formatting code here
+  return `$${amount.toFixed(2)}`
+}
