91f5abec4c9863b6bf636b9e66877dfea63a0d69
JaceLee <jaceleedev@gmail.com>
2023-03-30 13:10:33 +0900

    8kyu Do I get a bonus? 풀이 완료

---
+++ 8kyu/Logic/do-i-get-a-bonus?.js
@@ -0,0 +1,7 @@
+// 8 kyu Do I get a bonus? 문제
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+  // your code here
+  return bonus ? `£${salary * 10}` : `£${salary}`
+}
