d0d55ce36a825498c1d3f5777ce6dc266665c11c
JaceLee <jaceleedev@gmail.com>
2023-03-31 15:44:44 +0900

    8kyu Improving Math.round(x) 풀이 완료

---
+++ 8kyu/Logic/improving-math-round.js
@@ -0,0 +1,7 @@
+// 8 kyu Improving Math.round(x) 문제
+// https://www.codewars.com/kata/56be025f9347a066c7000e4f/train/javascript
+
+Math.roundTo = function (number, precision) {
+  // TODO: Program Me
+  return Number(number.toFixed(precision))
+}
