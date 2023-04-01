f63594599848f95ea88440b3907e12d121eb3948
JaceLee <jaceleedev@gmail.com>
2023-04-01 17:37:50 +0900

    8kyu My head is at the wrong end! 풀이 완료

---
+++ 8kyu/Arrays/my-head-is-at-the-wrong-end!.js
@@ -0,0 +1,7 @@
+// 8 kyu My head is at the wrong end! 문제
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
+
+function fixTheMeerkat(arr) {
+  //your code here
+  return arr.reverse()
+}
