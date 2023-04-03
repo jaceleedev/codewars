903ef19605b7312d8ac917d5dd5fb0b93596a2e3
JaceLee <jaceleedev@gmail.com>
2023-04-03 15:27:28 +0900

    8kyu pick a set of first elements 풀이 완료

---
+++ 8kyu/Arrays/pick-a-set-of-first-elements.js
@@ -0,0 +1,7 @@
+// 8 kyu pick a set of first elements 문제
+// https://www.codewars.com/kata/572b77262bedd351e9000076/train/javascript
+
+function first(arr, n = 1) {
+  // return arr;
+  return arr.slice(0, n)
+}
