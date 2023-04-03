d6a3b09b045e9a4359cc714a0f6135ba2b8929c7
JaceLee <jaceleedev@gmail.com>
2023-04-03 15:24:51 +0900

    8kyu You only need one - Beginner 풀이 완료

---
+++ 8kyu/Arrays/you-only-need-one-beginner.js
@@ -0,0 +1,7 @@
+// 8 kyu You only need one - Beginner 문제
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+
+function check(a, x) {
+  // your code here
+  return a.includes(x)
+}
