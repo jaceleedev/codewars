a5bb7812ff29378660545620c883d57de3e5e723
JaceLee <jaceleedev@gmail.com>
2023-04-05 15:45:35 +0900

    8kyu Remove exclamation marks 풀이 완료

---
+++ 8kyu/Strings/remove-exclamation-marks.js
@@ -0,0 +1,6 @@
+// 8 kyu Remove exclamation marks 문제
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '')
+}
