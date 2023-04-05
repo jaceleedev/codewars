a904c74432981b3f15060d9daff0027bec0f427e
JaceLee <jaceleedev@gmail.com>
2023-04-05 15:27:17 +0900

    8kyu Reversed Words 풀이 완료

---
+++ 8kyu/Strings/reversed-words.js
@@ -0,0 +1,6 @@
+// 8 kyu Reversed Words 문제
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ') // reverse those words
+}
