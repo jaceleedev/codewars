828fb5b6df4ddaddd37365796a5b5e71762d02cc
JaceLee <jaceleedev@gmail.com>
2023-04-05 15:14:24 +0900

    8kyu Contamination #1 -String- 풀이 완료

---
+++ 8kyu/Strings/contamination-#1-String.js
@@ -0,0 +1,7 @@
+// 8 kyu Contamination #1 -String- 문제
+// https://www.codewars.com/kata/596fba44963025c878000039/train/javascript
+
+function contamination(text, char) {
+  // Code here ;)
+  return char.repeat(text.length)
+}
