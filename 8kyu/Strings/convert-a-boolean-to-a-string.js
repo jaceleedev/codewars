7b6c2783f45b0c5bd04e0b429e4f4fcba7dda572
JaceLee <jaceleedev@gmail.com>
2023-04-05 15:25:21 +0900

    8kyu Convert a Boolean to a String 풀이 완료

---
+++ 8kyu/Strings/convert-a-boolean-to-a-string.js
@@ -0,0 +1,7 @@
+// 8 kyu Convert a Boolean to a String 풀이 완료
+// https://www.codewars.com/kata/551b4501ac0447료18f0009cd/train/javascript
+
+function booleanToString(b) {
+  //your code here
+  return b ? 'true' : 'false'
+}
