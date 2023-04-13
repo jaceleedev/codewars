b1524c35e4d03fdb45645604845f78f1afa32ba9
JaceLee <jaceleedev@gmail.com>
2023-04-13 10:05:19 +0900

    8kyu Returning Strings 풀이 완료

---
+++ 8kyu/Strings/returning-strings.js
@@ -0,0 +1,7 @@
+// 8 kyu Returning Strings 문제
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+function greet(name) {
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+}
