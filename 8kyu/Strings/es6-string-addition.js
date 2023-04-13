f6a0b40b8e8653601aaee7ccb46fc5fd4a5d5447
JaceLee <jaceleedev@gmail.com>
2023-04-13 09:51:03 +0900

    8kyu ES6 string addition 풀이 완료

---
+++ 8kyu/Strings/es6-string-addition.js
@@ -0,0 +1,7 @@
+// 8 kyu ES6 string addition 문제
+// https://www.codewars.com/kata/582e4c3406e37fcc770001ad/train/javascript
+
+function joinStrings(string1, string2) {
+  // code here
+  return `${string1} ${string2}`
+}
