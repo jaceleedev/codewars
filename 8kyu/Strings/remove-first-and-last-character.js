06fef8f6ca9f0c0e97bf16453a27811bbb5ae686
JaceLee <jaceleedev@gmail.com>
2023-04-13 10:01:55 +0900

    8kyu Remove First and Last Character 풀이 완료

---
+++ 8kyu/Strings/remove-first-and-last-character.js
@@ -0,0 +1,7 @@
+// 8 kyu Remove First and Last Character 문제
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+function removeChar(str) {
+  //You got this!
+  return str.slice(1, str.length - 1)
+}
