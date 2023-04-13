a616ca56838469d9d58942e124cca1348cd97076
JaceLee <jaceleedev@gmail.com>
2023-04-13 09:43:42 +0900

    If you can't sleep, just count sheep!!

---
+++ 8kyu/Strings/if-you-can't-sleep.js
@@ -0,0 +1,13 @@
+// 8 kyu If you can't sleep, just count sheep!! 문제
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/javascript
+
+const countSheep = function (num) {
+  //your code here
+  let answer = ''
+
+  for (let i = 1; i <= num; ++i) {
+    answer += `${i} sheep...`
+  }
+
+  return answer
+}
