849e6bb331eb3316265c07449767a29980561ca2
JaceLee <jaceleedev@gmail.com>
2023-04-14 11:22:36 +0900

    8kyu All Star Code Challenge #18 풀이 완료

---
+++ 8kyu/Strings/all-star-code-challenge-#18.js
@@ -0,0 +1,7 @@
+// 8 kyu All Star Code Challenge #18 문제
+// https://www.codewars.com/kata/5865918c6b569962950002a1/train/javascript
+
+function strCount(str, letter) {
+  //code here
+  return str.split(letter).length - 1
+}
